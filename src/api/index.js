49f802665d6ff0f2154e4ad0630a863404153ec7
Vision-Tey <nimusiimavision@gmail.com>
2024-05-29 00:50:53 +0300

    Change the backend base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-project-vi.herokuapp.com/'})
+const API = axios.create({ baseURL: 'https://memories-backend-sncr.onrender.com/'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
